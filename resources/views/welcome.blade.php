a770ad677834e53348b73b85572f57e20f584610
DesignerNik05 <wd.nikitadhiman@gmail.com>
2025-10-28 20:04:39 +0530

    include style file

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>RTM Trucking</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="../css/app.css">
+    <link rel="stylesheet" href="{{ asset('style-main.css') }}">
 </head>
 <body>
 <header id="main-header" class="web-header">
