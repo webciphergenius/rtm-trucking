ee8f0c89b75f169a312a4973bfa9d1e822405418
DesignerNik05 <wd.nikitadhiman@gmail.com>
2025-10-28 19:20:27 +0530

    Added style file and corrected the logo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>RTM Trucking</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-   
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 <body>
 <header id="main-header" class="web-header">
@@ -19,7 +19,7 @@
     </div>
     <nav class="navbar navbar-expand-sm">
         <div class="container">
-            <a class="navbar-brand" href="#">
+            <a class="navbar-brand" href="/">
                 <img src="{{ asset('images/web-logo.png') }}" alt="rtm-logo" height="50">
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
